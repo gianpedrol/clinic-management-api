b779991f65d21f92e8b1aba065b7cad26245c5bb
Gianfranco Pedrol <tech@provatis.academy>
2023-10-06 14:36:40 -0300

    first commit

---
+++ database/migrations/2023_10_04_174746_create_profissionals_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProfissionalsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profissionals', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id')->references('id')->onUpdate('NO ACTION')->onDelete('CASCADE');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profissionals');
+    }
+}
