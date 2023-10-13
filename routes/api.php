3ea1da2e223753bce5b64d4fb86ca4df1612b2d4
Gianfranco Pedrol <tech@provatis.academy>
2023-10-13 15:43:52 -0300

    criacao de endpoints

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\Atendimento\AtendimentoController;
 use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\Service\ServiceController;
 use App\Http\Controllers\User\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -31,4 +33,24 @@ Route::middleware('auth:api')->group(function () {
     /**Rotas de Usurario */
 
     Route::post('register/user', [UserController::class, 'registerUser']);
+    Route::get('lista/usuarios', [UserController::class, 'listUsers']);
+    Route::get('detalhes/usuario/{id}', [UserController::class, 'showUser']);
+    Route::delete('delete/user/{id}', [UserController::class,'deleteUser']);
+
+   /**Rotas de atendimento */
+
+   Route::post('criar/agendamento', [AtendimentoController::class, 'criarAtendimento']);
+   Route::post('cancelar/atendimento/{id}', [AtendimentoController::class, 'cancelarAtendimento']);
+   Route::post('finalizar/atendimento/{id}', [AtendimentoController::class, 'finalizarAtendimento']);
+   Route::get('lista/atendimentos/profissional', [AtendimentoController::class, 'listarAgendamentosProfissional']);
+   Route::get('lista/atendimentos/admin', [AtendimentoController::class, 'listarAtendimentosClinicas']);
+   Route::get('detalhe/atendimento/{id}', [AtendimentoController::class, 'detalhesAtendimento']);
+
+
+   /** Rotas de Serviço */
+
+   Route::post('adicionar/servico', [ServiceController::class, 'createService']);
+   Route::get('lista/servicos', [ServiceController::class, 'listServicos']);
+   Route::get('detalhe/servico/{id}', [ServiceController::class, 'showServico']);
+   Route::put('atualizar/servico/{id}', [ServiceController::class, 'updateServico']);
 });
